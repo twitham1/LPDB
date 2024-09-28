16c1b30b121bb70d0f01509081c9ab33e6c78753
Timothy D Witham <twitham@sbcglobal.net>
2024-09-28 01:17:51 -0500

    Fix multi-line Picasa descriptions

--- lib/LPDB/Picasa.pm
+++ lib/LPDB/Picasa.pm
@@ -28,7 +28,9 @@ sub ini_read {
     while (<$fh>) {
 	chomp;
 	s/\r*\n*$//;
-	s/\&\#(\d{3});/sprintf "%c", oct($1)/eg; # will this corrupt if we write it out later?
+	s/\\n/\n/g;			    # seen in Picasa description
+	s/\&\#(\d{3});/sprintf "%c", $1/eg; # seen in album description
+	s/\r\n/\n/g;			    # windows CRLF -> unix \n
 	if (/^\[([^\]]+)\]/) {
 	    $section = $1;
 	} elsif (my($k, $v) = split '=', $_, 2) {
@@ -45,14 +47,20 @@ sub ini_updatedb {
     my $dir = $ini->{dir};
     $dir =~ m@/$@ or return;
     my $schema = $self->schema;
-    my $id = LPDB::Filesystem::_savedirs($dir); # hack!!! cached dir id
+    my $id = $self->_savedirs($dir); # hack!!! cached dir id
     print "in $dir = $id is following:\n";
     for my $k (keys %$ini) {
 	my $this = $ini->{$k};
-	if ($k =~ /^Picasa/) {
-	    for my $id (keys %{$this}) {
-		print "$k\t{$id}{$this->{$id}}\n";
-	    }
+	if ($k eq 'dir') {
+	    next;
+	} elsif ($k =~ /^Picasa/) {
+	    # ('name' => 'test',
+	    #  'P2category' => 'Folders on Disk',
+	    #  'date' => '43258.394167',
+	    #  'description' => 'description (optional)
+	    #  can be multi-line
+	    #  like this...',
+	    #  'location' => 'Place taken (optional)')
 	} elsif ($k =~ /^Contacts2/) {
 	    for my $id (keys %{$this}) {
 		my($name, $email) = split ';', $this->{$id};
@@ -67,38 +75,40 @@ sub ini_updatedb {
 		    $tmp eq $name or warn "$dir: $tmp -> $name";
 		} else {
 		    $obj->hexid($id);
-		    $obj->name($name);
+p		    $obj->name($name);
 		    $obj->email($email);
 		    $obj->insert;
 		}
 	    }
 	} elsif ($k =~ /^\.album:(\w+)$/) {
-	    print qq'{album}{$1} = 1;\n';
-	    for my $id (keys %{$this}) {
-		print "$k\t{$id}{$this->{$id}}\n";
-	    }
-	    # $pic->{album}{$1} =
-	    # 	&_merge(undef, $pic->{album}{$1}, $this->);
-	} elsif ($k eq 'dir') {
-	    next;
+	    # ('.album:4b5914837de8a11f7029631a2c9280f9' => (
+	    # 	 'location' => 'Place taken (optional)',
+	    # 	 'name' => 'test - album of kids',
+	    # 	 'description' => 'Description (optional)
+
+	    # 	 can be multi-line
+	    # 	 like this...',
+	    # 	 'token' => '4b5914837de8a11f7029631a2c9280f9',
+	    # 	 'date' => '2018-06-07T09:27:35-05:00'
+	    #  ))
 	} elsif (my $row = $schema->resultset('Picture')->find(
 		     { dir_id => $id,
 		       basename => $k })) { # image
-	    for my $id (keys %{$this}) {
-		print "$k\t{$id}{$this->{$id}}\n";
-	    }
-	    print "\tsetting star in $dir/$k\n";
+	    # for my $id (keys %{$this}) {
+	    # 	print "$k\t{$id}{$this->{$id}}\n";
+	    # }
+	    # print "\tsetting star in $dir/$k\n";
 	    $row->stars($this->{star} && $this->{star} eq 'yes' ? 1 : 0);
 	    $row->is_changed
 		? $row->update
 		: $row->discard_changes;
-	} elsif (-f "$dir$k") {	# image not in database?!
+	} elsif (-f "$dir$k") {	# should not fail to find in previous!!!
 	    print "WHY IS $dir$k not in DB?!!!!!!!!!!!!!\n";
 	    for my $id (keys %{$this}) {
 		print "$k\t{$id}{$this->{$id}}\n";
 	    }
 	} else {
-	    print "[$k] ignored\n";
+	    print "[$k] no such file, ignored\n";
 	}
     }
 }
